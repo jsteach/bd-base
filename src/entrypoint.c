a72282cde56717c4b833808fb2f7b6f71fec08e5
00547 <00547@bart.ca>
2025-02-09 17:56:04 -0500

    Started amalgamation example

---
+++ src/entrypoint.c
@@ -0,0 +1,16 @@
+#include "entrypoint.h"
+// #include "raylib.h"
+#include "knob.h"
+
+void raylib_start(void){
+    // InitWindow(960,540,"base de donnée");
+    // SetTargetFPS(60);
+    // while(!WindowShouldClose()){
+    //     ClearBackground(RED);
+    //     BeginDrawing();
+    //     //TODO: Do stuff
+    //     EndDrawing();
+    // }
+
+    // CloseWindow();
+}
