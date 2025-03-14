794a950b0e0b8207a888cb12099daa9b491ed309
00547 <00547@bart.ca>
2025-03-13 20:50:07 -0400

    Add comments and easy setup for zig

--- src/entrypoint.c
+++ src/entrypoint.c
@@ -7,29 +7,47 @@
 
 #define LOG_SQLITE3_ERROR(db) knob_log(KNOB_ERROR, "%s:%d: SQLITE3 ERROR: %s\n", __FILE__, __LINE__, sqlite3_errmsg(db))
 
+/*
+* Fonction qui affiche des fruits spécifique, on utilise une valeur passé en paramètre
+* pour donner une valeur à un ? dans une requêtes SQL.
+*/
 void see_state_fruits(sqlite3* db,char* state_name){
     sqlite3_stmt* stmt = NULL;
     char content[] =
     "SELECT fruit,price\n"
     "FROM FruitsForSale\n"
-    "WHERE etat=?;";
+    "WHERE etat=?;";// <---------------IMPORTANT: Le ? seras défini plus tard avec la fonction bind_text (ligne 33)
     int ret = sqlite3_prepare_v2(db,content,-1,&stmt,NULL);
 
     if(ret != SQLITE_OK){
         LOG_SQLITE3_ERROR(db);
     }
-
+    /*
+    IMPORTANT:
+        Est nécessaire à la requête car elle a un ?. On peut avoir plusieurs ?
+        dans une requête donc nous aurons alors besoin d'autant de bind que de ?.
+        Si vous ne binder pas de valeur au ?, il y aura une erreur.
+        Si on a plusieurs ?, les index des ? seront 1,2,3,etc. Les index devront être
+        spécifiés après le paramètre statement à chaque opération de bind.
+        Voir exemple, add_random_fruit.
+    */
     if(sqlite3_bind_text(stmt,1,state_name,-1,SQLITE_STATIC) != SQLITE_OK){
         LOG_SQLITE3_ERROR(db);
     }
 
+    /*
+    Ici On créer une boucle parce que les requêtes SQL vont retourner plusieurs ranger exemple:
+        Orange|0.85 --- Première ranger
+        Lemon|1.25  --- Deuxième ranger
+    La boucle continue tant que step retourne une ranger(row)
+    */
     for(int ret = sqlite3_step(stmt);ret != SQLITE_DONE;ret = sqlite3_step(stmt)){
         if(ret != SQLITE_ROW){
             LOG_SQLITE3_ERROR(db);
         }
         int column = 0;
-        const unsigned char* fruit = sqlite3_column_text(stmt,column++);
-        float price = sqlite3_column_double(stmt,column);
+        const unsigned char* fruit = sqlite3_column_text(stmt,column++); //Permet d'aller chercher le nom du fruit
+        float price = sqlite3_column_double(stmt,column); //Permet d'aller chercher le prix du fruit
         knob_log(KNOB_INFO,"Etat %s a le fruit %s avec le prix %.2f$",state_name,fruit,price);
     }
 
@@ -50,6 +68,10 @@ const char* states[] = {
     "HA"
 };
 void add_random_fruit(sqlite3* db){
+    /*
+        Commence une TRANSACTIONS SQL. sqlite3_exec permet d'exécuter
+        une requête SQL qui n'a pas à être préparée vu quel n'a pas de valeur à recevoir du programme C.
+    */
     if (sqlite3_exec(db, "BEGIN;", NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(db);
         return;
@@ -73,6 +95,10 @@ void add_random_fruit(sqlite3* db){
         LOG_SQLITE3_ERROR(db);
     }
     ret = sqlite3_step(stmt);
+    /*
+        Ici on fait encore une requête SQL qui roule le SQL directement,
+        mais maintenant on termine la TRANSACTION avec le COMMIT.
+    */
     if (sqlite3_exec(db, "COMMIT;", NULL, NULL, NULL) != SQLITE_OK) {
         LOG_SQLITE3_ERROR(db);
     }
